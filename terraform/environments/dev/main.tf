6ed51f026a9e00158c3f3c5ce877cbabcb06081b
Casp3r-0 <michzadc@gmail.com>
2025-03-20 15:19:02 -0500

    update: fix values

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,51 +1,47 @@
 module "network" {
   source       = "../../modules/network"
-  project_id   = var.project_id
+  project_id   = module.network.project_id
   network_name = "dev-network"
-  subnets = []
+  subnets      = []
 }
 
 module "artifact_registry" {
-  source       = "../../modules/artifact-registry"
-  project_id   = var.project_id
-  location     = "us-central1"
+  source        = "../../modules/artifact-registry"
+  location      = "us-central1"
   repository_id = "dev-repository"
 }
 
 module "iam" {
-  source           = "../../modules/iam"
-  project_id       = var.project_id
+  source             = "../../modules/iam"
   service_account_id = "dev-sa"
-  roles            = []
+  roles              = []
 }
 
 module "static_ip" {
-  source      = "../../modules/static-ip"
-  project_id  = var.project_id
-  name        = "dev-static-ip"
-  region      = "us-central1"
+  source = "../../modules/static-ip"
+  name   = "dev-static-ip"
+  region = "us-central1"
 }
 
 module "persistent_storage" {
-  source    = "../../modules/persistent-storage"
-  project_id = var.project_id
-  name      = "dev-disk"
-  zone      = "us-central1-a"
+  source = "../../modules/persistent-storage"
+  name   = "dev-disk"
+  zone   = "us-central1-a"
 }
 
 module "gke" {
-  source         = "../../modules/gke"
-  project_id     = var.project_id
-  cluster_name   = "dev-cluster"
+  source           = "../../modules/gke"
+  cluster_name     = "dev-cluster"
   cluster_location = "us-central1"
-  network_name   = module.network.network_name
-  subnetwork_name = "dev-subnet"
+  subnetwork_name  = "dev-subnet"
 }
 
-module "node_pool" {
-  source         = "../../modules/node-pool"
-  project_id     = var.project_id
-  cluster_name   = module.gke.cluster_name
-  cluster_location = module.gke.cluster_location
-  node_pool_name = "dev-node-pool"
+module "node-pool" {
+  source             = "../../modules/node-pool"
+  node_pool_name     = "dev-node-pool"
+  min_nodes          = module.node-pool.min_nodes
+  max_nodes          = module.node-pool.max_nodes
+  machine_type       = module.node-pool.machine_type
+  initial_node_count = module.node-pool.initial_node_count
+
 }
